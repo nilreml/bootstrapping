c07efe2eb768fdedeb2a4f57b4ab3bd718437c88
nilreml <mono.liner@gmail.com>
2020-06-06 13:32:07 +0200

    test

--- debian.sh
+++ debian.sh
@@ -2,17 +2,17 @@
 
 USERNAME=$(whoami)
 
-SUDOERS_PATH=/etc/sudoers.d
-SUDOERS_FILE=${USERNAME}
+SUDOERS_PATH="/etc/sudoers.d"
+SUDOERS_FILE="${USERNAME}"
 SUDOERS_STRING="${USERNAME} ALL=(ALL:ALL) NOPASSWD:ALL"
 
-APT_SOURCES_NAME=buster-backports
-APT_SOURCES_MIRROR=http://mirror.netcologne.de/debian/
-APT_SOURCES_PATH=/etc/apt/sources.list.d
-APT_SOURCES_FILE=${APT_SOURCES_NAME}.list
-APT_SOURCES_STRING=${APT_SOURCES_NAME} main contrib non-free
-APT_SOURCES_STRING_1=deb ${APT_SOURCES_STRING}
-APT_SOURCES_STRING_2=deb-src ${APT_SOURCES_STRING}
+APT_SOURCES_NAME="buster-backports"
+APT_SOURCES_MIRROR="http://mirror.netcologne.de/debian/"
+APT_SOURCES_PATH="/etc/apt/sources.list.d"
+APT_SOURCES_FILE="${APT_SOURCES_NAME}.list"
+APT_SOURCES_STRING="${APT_SOURCES_NAME} main contrib non-free"
+APT_SOURCES_STRING_1="deb ${APT_SOURCES_STRING}"
+APT_SOURCES_STRING_2="deb-src ${APT_SOURCES_STRING}"
 
 su -c "
 echo ${SUDOERS_STRING}>${SUDOERS_PATH}/${SUDOERS_FILE};
