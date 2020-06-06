bc7fd915d9bc2fa127f4db0eb921b1d311a6c2a8
nilreml <mono.liner@gmail.com>
2020-06-06 14:22:23 +0200

    test

--- debian.sh
+++ debian.sh
@@ -10,18 +10,20 @@ APT_SOURCES_NAME="buster-backports"
 APT_SOURCES_MIRROR="http://mirror.netcologne.de/debian/"
 APT_SOURCES_PATH="/etc/apt/sources.list.d"
 APT_SOURCES_FILE="${APT_SOURCES_NAME}.list"
-APT_SOURCES_STRING="${APT_SOURCES_NAME} main contrib non-free"
+APT_SOURCES_STRING="${APT_SOURCES_MIRROR} ${APT_SOURCES_NAME} main contrib non-free"
 APT_SOURCES_STRING_1="deb ${APT_SOURCES_STRING}"
 APT_SOURCES_STRING_2="deb-src ${APT_SOURCES_STRING}"
 
-su -c "
-echo '${SUDOERS_STRING}'>'${SUDOERS_PATH}/${SUDOERS_FILE}';
-usermod -aG sudoers ${USERNAME};
-echo ${APT_SOURCES_STRING_1}>${APT_SOURCES_PATH}/${APT_SOURCES_FILE}
-echo ${APT_SOURCES_STRING_2}>${APT_SOURCES_PATH}/${APT_SOURCES_FILE}
-apt autoclean
-apt update
-apt dist-upgrade -y
-apt autoremove --purge -y
-apt install vim git
-"
+su -c "echo '${SUDOERS_STRING}'>'${SUDOERS_PATH}/${SUDOERS_FILE}'"
+
+sudo echo test
+
+#usermod -aG sudoers ${USERNAME};
+#echo ${APT_SOURCES_STRING_1}>${APT_SOURCES_PATH}/${APT_SOURCES_FILE}
+#echo ${APT_SOURCES_STRING_2}>${APT_SOURCES_PATH}/${APT_SOURCES_FILE}
+#apt autoclean
+#apt update
+#apt dist-upgrade -y
+#apt autoremove --purge -y
+#apt install vim git
+#"
